5f8de1296421a09675ede1373cb185fd35f56d44
Sadjoaldi <alsannealdi@outlook.com>
2023-10-28 01:45:23 +0200

    beging themealdb281023

---
+++ src/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function About(props) {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor
+        voluptatem molestias doloribus similique. Excepturi velit, ducimus ea,
+        cumque deserunt ullam sit suscipit quaerat, similique ad hic fuga est
+        distinctio quibusdam. Delectus impedit porro provident illum nulla!
+        Quaerat exercitationem voluptates doloremque, impedit ipsum, sit
+        deleniti, aspernatur quam odit ratione vitae dignissimos.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut odit vitae
+        iusto ab, mollitia dolorem consequuntur laudantium reiciendis nulla
+        autem voluptas labore recusandae iste fugiat?
+      </p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+function Home(props) {
+  return (
+    <div>
+      <h1>Accueil</h1>
+    </div>
+  );
+}
+
+export default Home;
